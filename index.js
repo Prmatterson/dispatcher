7057f7366c0814e557243ae7be040bba8fe0ad9e
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2022-06-23 21:13:46 -0230

    Table Function Created

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
-let rows = [ // Learn more about 2D arrays
-    ["Time", "Tech 1", "Tech 2", "Tech 3"],
-    [8.0, "", "", ""], 
-    [8.1, "", "", ""], 
-    [8.2, "", "", ""], 
-    [8.3, "", "", ""]
-];
+function createRow(tableObj, tableData) {
+    for (let row_index = 0; row_index < tableData.length; row_index++) {
+        createCell(tableObj, tableData, row_index);
+    }
+}
 
+function cellOnClick(cellContent) {
+    let logMsg = "The content is " + cellContent;
+    alert(logMsg); // Creates a popup
+    console.log(cellObj); // Sends content to the debug viewer
+}
 
-//LEARN ABOUT FUNCTIONS
-//let newTable1 = createTable("table-id-name-1", content_2d_array);
-//let newTable2 = createTable("table-id-name-2", content_2d_array);
-//let newTable3 = createTable("table-id-name-3", content_2d_array);
-// function createTable(tableId, table2dArrContent){
-//     let tableObj = document.createElement("table"); 
-//     //Create table using for loops
-//     return tableObj;
-// }
-
-//Create table
-let jobTable = document.createElement("table"); // html object <table><table/>
-// let myContiner = document.createElement("p"); // html object <p><p/>
-// let myTable = document.createElement("div"); // html object <div><div/>
-// let myTable = document.createElement("h1"); // html object <h1><h1/>
-
-
-
-for (let row_index = 0; row_index < rows.length; row_index++) {
+function createCell(tableObj, tableData, row_index) {
     // Go through each of the rows
     // Extract each row array
-    const single_row_content = rows[row_index];
-    // Create row
-    let row = jobTable.insertRow();
+    let singleRowContent = tableData[row_index];
+    //Create row
+    let rowObj = tableObj.insertRow();
 
-    for (let column_index = 0; column_index < single_row_content.length; column_index++) {
+    for (let columnIndex = 0; columnIndex < singleRowContent.length; columnIndex++) {
         // Go through each of the cells
-        // Extract the cell content from the row array 
-        const single_cell_content = single_row_content[column_index];
+        // Extract the cell content from the row array
+        let singleCellContent = singleRowContent[columnIndex];
         // Create cell
-        let cell = row.insertCell();
+        let cellObj = rowObj.insertCell();
         // Add the cell content to cell object
-        cell.innerHTML = single_cell_content;
-        // Click event
-        cell.onclick = function(){
-            let log_msg = "The content is "+ single_cell_content;
-            // alert(log_msg); // Creates a popup
-            console.log(cell); // Sends content to the debug viewer
-        }
-    } 
+        cellObj.innerHTML = singleCellContent;
+        //Click event
+        cellObj.onclick = cellOnClick
+    }
 }
-//Assign the table JS object to the html object
-document.getElementById("job-table").appendChild(jobTable);
+function createHeader(tableHeaderData, tableObj) {
+    let headerRowObj = tableObj.insertRow();
+    for (let i = 0; i < tableHeaderData.length; i++) {
+        let headerCell = document.createElement("TH");
+        headerCell.innerHTML = tableHeaderData[i];
+        headerRowObj.appendChild(headerCell);
+    }
+}
+
+function createTable(tableData, tableHeaderData, tableId) {
+    let tableObj = document.createElement("table"); // html object <table><table/>
+    createHeader(tableHeaderData, tableObj);
+    createRow(tableObj, tableData);
+
+    //Assign the table JS object to the html object
+    document.getElementById(tableId).appendChild(tableObj);
+    return tableObj;
+}
+
+function onReady() {
+    let tableHeaderData1 = ["H1", "H2", "H3", "H4"]
+    let tableData1 = [
+        [1, 4, 6, 6],
+        [9, 7, 8, 9]
+    ]
+    
+    let tableHeaderData2 = ["H1", "H2", "H3", "H4"]
+    let tableData2 = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ];
+
+    let tableObj1 = createTable(tableData1, tableHeaderData1, "test-table");
+    let tableObj2 = createTable(tableData2, tableHeaderData2, "evens-odds");
+
+}
+document.addEventListener("DOMContentLoaded", onReady);
